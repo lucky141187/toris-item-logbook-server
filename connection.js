151294359296540aec7964588959c9652c7e942c
lucky141187 <lucky141187@gmail.com>
2025-05-09 23:05:25 +0400

    Update connection.js

--- connection.js
+++ connection.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql')
 
 const db = mysql.createConnection({
-    host : "localhost",
-    user : "root",
-    password : "",
-    database : "torishimajoblist"
+    host : "127.0.0.1",
+    port : 3306,
+    user : "u275669251_lucky141187",
+    password : "Lucian~7~",
+    database : "u275669251_torishimajobdb",
     
 })
 
